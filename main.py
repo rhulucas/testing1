9e0c107ae03811e371c428409e11b03e8892402f
Rong Hu <144379767+rhulucas@users.noreply.github.com>
2023-12-18 03:18:39 +0000

    create app to get language review changes

--- main.py
+++ main.py
@@ -16,3 +16,22 @@ def get_movieview_by_name(movie_name: str):
         movies.youtube_search(max_result=10)
         return movies._review_videos
     return results
+
+@app.get("/descriptions/{video_id}")
+def get_video_description(video_id: str):
+     movie_new= Movie ("Some Title ")
+     movie_description = movie_new.youtube_description(video_id)
+     if movie_description:
+         return movie_description
+     else:
+        raise HTTPException(status_code=404, detail="Description not found")
+      
+@app.get("/moviereviews/{language}/{movie_name}")
+def get_find_movie_review_videos(language:str, movie_name:str):
+    results = []
+    if movie_name:
+        moviesA = Movie(movie_name)
+        translated_title = moviesA.find_movie_review_videos(language, movie_name)
+        moviesA.youtube_search(max_result=10)
+        return moviesA._review_videos
+    return results
