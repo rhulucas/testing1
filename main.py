248c6d9d03ac00e3b834a83e1c8e39ed35b92123
Rong Hu <144379767+rhulucas@users.noreply.github.com>
2023-12-18 02:21:56 +0000

    The input should be a movie title, and the output should be a list of 10 movie reviews and commentaries about that movie.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from movie import Movie
 
 app =FastAPI()
 #video_id = MovieID()
@@ -6,3 +7,12 @@ app =FastAPI()
 @app.get("/")
 def root():
     return{"message": "Hello World"}
+
+@app.get("/movie/{movie_name}")
+def get_movieview_by_name(movie_name: str):
+    results = []
+    if movie_name:
+        movies = Movie(movie_name)
+        movies.youtube_search(max_result=10)
+        return movies._review_videos
+    return results
